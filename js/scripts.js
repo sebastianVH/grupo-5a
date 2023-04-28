98d016fbcc9652585a84875a71365bfe030936ff
sebastianVH <114303713+sebastianVH@users.noreply.github.com>
2023-04-28 19:17:25 -0300

    clase 28/04

--- js/scripts.js
+++ js/scripts.js
@@ -208,3 +208,171 @@ console.log(usuario)
 if (!usuario) { 
     console.log("usuario vacio");
 }
+
+/* CLASE jS 3 */
+
+//?switch: COMPARA CASOS en base al resultado que hayamos pasado
+
+//* va a ser un comercio, el cual vende partes de computadora. Segun la opcion del usuario, sera el componente que agrega al carrito
+
+let eleccion;
+
+eleccion = 0
+
+let variable = 1
+switch (eleccion) {
+    
+    case 1:
+        console.log("Agregamos mouse al carrito.");
+        break; //! es importante agregarlo para terminar la ejecucion del switch
+    case 2:
+        console.log("Agregamos un monitor al carrito.");
+        break;
+    case 3:
+        console.log("Agregamos un teclado al carrito.");
+        break;
+    default:
+        console.log("Saliendo del programa.");
+}
+
+//? Array: una estructura que contine dentro varios elementos
+
+//* como creamos un array?
+
+let mi_array = [] //* aqui creamos un array vacio el cual tiene 0 posiciones designadas
+let array_2 = Array(6) ///*aqui creamos un array que tiene 6 posiciones designada
+
+//indices   0 1 2 3 4 5 6 7  8  9 10
+mi_array = [2,5,7,5,9,"durazno","pollo",4,1,3,19,14,8]
+console.log(mi_array[5]);
+//* metodos para el array
+
+console.log(mi_array.length); //*nos indica el largo del array
+
+//*agreguemos elementos al array
+
+mi_array.push("Hola") //* nos permite agregar un elemento AL FINAL DEL ARRAY
+mi_array.unshift("Mundo") //* nos permite agregar un elemento AL COMIENZO DEL ARRAY
+
+//*eliminar elementos
+mi_array.pop() //* elimina el ultimo elemento de la lista
+mi_array.shift() //* elimina el primer elemento de la lista
+
+//* agregar, reemplazar o eliminar
+//? parametro 1: eliminar TODOS LOS ELEMENTOS desde el indice indicado
+//? parametro 2: indica la cantidad de elementos a eliminar
+//? parametro 3: nos indica un elemento que podremos insertar en el array. podemos insertar MUCHOS si queremos
+
+mi_array.splice(6,3,"Durazno","banana","pera","sandia","melon")
+
+//?tostring(): transforma un array a un string
+let array_string = mi_array.toString()
+console.log(array_string);
+
+//?slice(): crea un array a partir de una pieza de otro array
+//* parametro 1, nos indica el indice que comenzamos a copiar el nuevo array
+//* parametro 2, nos dice las posiciones que vamos a avanzar en el array 
+
+let array_cortado = mi_array.slice(4,12)
+console.log(array_cortado);
+
+//?includes(): esto nos permite saber si un elemento esta en el array. Y devuelve true si lo encuentra. Caso contrario, devuelve false
+console.log(mi_array.includes("uva"));
+
+//?concat(): unir los Array y creamos uno nuevo. 
+
+let animales1 = ["perro","gato"]
+let animales2 = ["caballo","vaca"]
+let animales3 = ["oveja","pollo"]
+let granja = animales1.concat(animales2,animales3)
+console.log(granja);
+
+//?sort() ordena los elementos de un array, segun su ubicacion de tabla
+console.log(mi_array);
+mi_array.sort() 
+
+//?indexOf(): nos devuelve la posicion de indice de un elemento buscado
+
+let array_frutas = ["Banana","Manzana","Durazno","Pera","Sandia"]
+console.log(array_frutas.indexOf("Naranja")); 
+//! si el index no encuentra el elemento dentro del array, NOS DEVUELVE -1
+
+//?reverse(): tomar el array original e invertir el orden
+
+array_frutas.reverse()
+array_frutas
+
+//*METODOS DE ARRAY CON FUNCIONES
+
+//?some(): busca un elemento dentro del Array BAJO UNA CONDICION. Si encuentra POR LO MENOS uno, nos devuelve true, sino sera false.
+
+let array_nombres = ["Seba","Mateo","Lara","Martin","Karina","Luis","Juan"]
+
+let array_numeros = [3,5,9,13,24,4,8,30]
+
+let algunoMayorA = array_numeros.some((numero) => numero > 10 )
+console.log(algunoMayorA);
+
+//?every(): chequea que TODOS LOS ELEMENTOS cumplan la condicion dada. Devuelve un booleano
+
+array_numeros
+
+let todosMenoresA = array_numeros.every((numero) => numero < 40)
+console.log(todosMenoresA);
+
+//? findIndex(): nos devuelve el PRIMER INDICE de posicion que cumpla con la condicion solicitada. Si no lo encuentra, devuelve -1
+
+array_nombres
+
+let indiceDe = array_nombres.findIndex((nombre) => nombre[0] === "N") //*Nombres que empiecen con N
+console.log(indiceDe);
+
+//? filter(): nos crea un nuevo array con todos los elementos que cumplan una condicion dada
+
+array_numeros
+
+let numerosMayoresA = array_numeros.filter((numeros) => numeros%2 === 0) //* Numeros que sean PARES
+console.log(numerosMayoresA);
+
+//? map(): Nos crea un nuevo array. nos permite es aplicarle una funcion a cada elemento.
+
+array_nombres
+
+let nombresMayuscula = array_nombres.map((nombre) => nombre.toUpperCase())
+console.log(nombresMayuscula);
+
+//? reduce(): nos crea un nuevo Array con un unico elemento, en base a alguna funcion aplicada.
+
+array_numeros
+
+let sumaNumeros = array_numeros.reduce((sumatoria,numeros) => sumatoria +numeros )
+console.log(sumaNumeros);
+
+//? from(): Nos permite crear un nuevo Array, a partir de otro array. 
+
+array_numeros
+
+let arrayPotencia = Array.from(array_numeros,(numero) => numero**2 )
+console.log(arrayPotencia);
+
+
+//! CASOS DE USO
+
+let palabra = "Cartuchera"
+
+let arrayUpperFrom = Array.from(palabra,(letra) => letra.toUpperCase())
+console.log(arrayUpperFrom);
+
+let palabraMayuscula = arrayUpperFrom.reduce((palabra,letra) => palabra+letra)
+console.log(palabraMayuscula);
+
+//*numeros sucesivos: vamos a tomar cada numero del string, multipicarlos por 2 , y luego sumarlos
+
+let string_numeros = "2531442"
+
+let arrayNumerosSeparados = Array.from(string_numeros,(numero) => numero*2)
+console.log(arrayNumerosSeparados);
+
+let arraySumaNumeros = arrayNumerosSeparados.reduce((total,numero) => numero + total)
+console.log(arraySumaNumeros);
+console.log(typeof arraySumaNumeros);
